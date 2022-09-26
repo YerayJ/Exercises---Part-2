dd3cb97ca228eb63406a0d69cc80f43b241d1dbf
YerayJ <114072332+YerayJ@users.noreply.github.com>
2022-09-26 09:42:29 +0200

    Add files via upload

---
+++ Exercise4.java
@@ -0,0 +1,35 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class Exercise4 {
+
+	public static void main(String[] args) {
+		// declare variables
+		int x, y,i;
+		Scanner keyboard = new Scanner (System.in);
+		// enter values
+		System.out.print("Enter value for x ");
+		x = keyboard.nextInt();
+		System.out.print("Enter value for y ");
+		y = keyboard.nextInt();
+		// code attempting to swap two variables
+		i = x;
+		x = y;
+		y = i;
+		
+		//display results
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+		}
+/*
+ * Can you see why the program doesn’t do what we hoped?
+ * Sí, ya que faltaba el Scanner y no recogía los datos del usuario.
+ * What would be the actual output of the program?
+ * Las dos variables dan el mismo valor.
+ * How could you modify the program above so that the values of the two variables are swapped successfully?
+ * Añadiendo el scanner.
+ * 
+ */
+
+}
